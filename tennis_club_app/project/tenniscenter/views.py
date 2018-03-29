b8bd216eed42b92c0cf0b82a816a8f36f0ec6f6d
Peyman <peyman.bateni@hotmail.com>
2018-03-28 19:43:21 -0700

    GUI added :)

--- tennis_club_app/project/tenniscenter/views.py
+++ tennis_club_app/project/tenniscenter/views.py
@@ -12,9 +12,9 @@ def index(request):
 	View function for home page of site.
 	"""
 	# Generate counts of some of the main objects
-	num_customers=Customers.objects.all().count()
-	num_instructors=Instructors.objects.all().count()
-	num_membership_plans=MembershipPlans.objects.count()  # The 'all()' is implied by default.
+	num_customers = Customers.objects.all().count()
+	num_instructors = Instructors.objects.all().count()
+	num_membership_plans = MembershipPlans.objects.count()  # The 'all()' is implied by default.
 
 	selectInstructors = SelectInstructors();
 	joinQuery = JoinQuery();
